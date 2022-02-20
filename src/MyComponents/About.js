415f46dcb568f352bb5e2bc60244d629859a1303
Madhur <68078506+madhur89@users.noreply.github.com>
2022-02-20 22:07:35 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+           <p>This Todo list app stores your todo to local strorage and we can edit delete todos. And it is made with ReactJS</p>
+        </div>
+    )
+}
